aa00c751d6ae1bf8190b6b2c951ea7d86f6b202c
Quang Dung <ntquangdung2001@gmail.com>
2023-04-07 17:43:53 +0700

    [Add  source code] footer header]

---
+++ src/layouts/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import BannerFooter from './BannerFooter'
+import BodyFooter from './BodyFooter'
+
+const Footer = () => {
+  return (
+    <div  className='footer'>
+     <BodyFooter></BodyFooter>
+     <BannerFooter></BannerFooter>
+    </div>
+  )
+}
+
+export default Footer
