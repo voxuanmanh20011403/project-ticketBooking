e013077ce709f2aa2d608308ddad1deba0b6b564
LV NT <levnhattan@gmail.com>
2023-04-10 17:03:11 +0700

    [Modified] UI Booking page

--- src/layouts/Booking/Trip.jsx
+++ src/layouts/Booking/Trip.jsx
@@ -5,7 +5,6 @@ import Container from "@mui/material/Container";
 import { styled } from "@mui/material/styles";
 import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
-import Typography from "@mui/material/Typography";
 import InfoTrip from "./InfoTrip";
 import TextField from "@mui/material/TextField";
 import SearchIcon from "@mui/icons-material/Search";
@@ -14,7 +13,11 @@ import FormGroup from "@mui/material/FormGroup";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import Checkbox from "@mui/material/Checkbox";
 import Skeleton from "@mui/material/Skeleton";
-
+import Breadcrumbs from "@mui/material/Breadcrumbs";
+import Typography from "@mui/material/Typography";
+import Link from "@mui/material/Link";
+import Stack from "@mui/material/Stack";
+import NavigateNextIcon from "@mui/icons-material/NavigateNext";
 // redux
 import { useDispatch, useSelector } from "react-redux";
 import { fetchTrips } from "redux/slices/tripsSilce";
@@ -29,6 +32,21 @@ const Item = styled(Paper)(({ theme }) => ({
 }));
 
 const label = { inputProps: { "aria-label": "Checkbox demo" } };
+// Breadcrumbs
+function handleClick(event) {
+  event.preventDefault();
+  // console.info("You clicked a breadcrumb.");
+}
+const SkeletonCustom = () => {
+  return (
+    <Stack  spacing={1}  direction="row">
+      {/* For other variants, adjust the size with `width` and `height` */}
+      {/* <Skeleton variant="circular" width={40} height={40} /> */}
+      <Skeleton variant="rectangular" width={210} height={180} />
+      <Skeleton variant="rounded" width={700} height={210} />
+    </Stack>
+  );
+};
 
 const Trip = ({ fetchData }) => {
   const [searchValue, setSearchValue] = useState("");
@@ -59,18 +77,51 @@ const Trip = ({ fetchData }) => {
       );
     }
   });
-  console.log("filteredData: " + filteredData);
+  console.log("filteredData: " + JSON.stringify(filteredData));
+
+  const breadcrumbs = [
+    <Link underline="hover" key="1" color="inherit" onClick={handleClick}>
+      Home
+    </Link>,
+    <Link underline="hover" key="2" color="inherit" onClick={handleClick}>
+      Tìm vé
+    </Link>,
+    <Typography key="3" color="text.primary">
+      Tên chuyến đi !!!!
+    </Typography>,
+  ];
+  // CircularProgress for {fetchData.length}
+  const [showNumber, setShowNumber] = useState(false);
+
+  setTimeout(() => {
+    setShowNumber(true);
+  }, 4000);
+
   return (
     <React.Fragment>
       <CssBaseline />
-      <Container fixed>
-        <Box sx={{ bgcolor: "#dfe4ea", height: "100vh" }}>
+      <div className="ctn__trip">
+        <Container className="breadcrumb" maxWidth="lg">
+          <Stack spacing={2}>
+            <Breadcrumbs
+              separator={<NavigateNextIcon fontSize="small" />}
+              aria-label="breadcrumb"
+            >
+              {breadcrumbs}
+            </Breadcrumbs>
+          </Stack>
+        </Container>
+
+        <Container maxWidth="lg">
           <Grid container spacing={2}>
+            <Grid item xs={8} className="form__search">
+              Đây là chỗ tìm kiếm gồm nơi đi nơi đến và datetimepicker của Dũng
+            </Grid>
             <Grid item xs={6}></Grid>
             <Grid item xs={6}>
               <TextField
                 variant="outlined"
-                placeholder="Search"
+                placeholder="Search..."
                 size="medium"
                 fullWidth
                 InputProps={{
@@ -83,44 +134,70 @@ const Trip = ({ fetchData }) => {
                 onChange={handleSearch}
               />
             </Grid>
+            <Grid item xs={10}></Grid>
             <Grid item xs={4}>
-              <Item>
-                Lọc theo loại xe:
+              <Item className="cnt__filter">
+                <Typography className="filter">Lọc</Typography>
                 <FormGroup>
+                  <Typography className="">Theo loại xe:</Typography>
                   <FormControlLabel
                     control={<Checkbox {...label} />}
                     label="Xe giường nằm 22 chỗ"
                     value="Xe giường nằm 22 chỗ"
                     onChange={handleCheckboxChange}
-                    checked={selectedCheckboxes.includes("Xe giường nằm 22 chỗ")}
+                    checked={selectedCheckboxes.includes(
+                      "Xe giường nằm 22 chỗ"
+                    )}
                   />
                   <FormControlLabel
                     control={<Checkbox {...label} />}
                     label="Xe giường nằm 34 chỗ"
                     value="Xe giường nằm 34 chỗ"
                     onChange={handleCheckboxChange}
-                    checked={selectedCheckboxes.includes("Xe giường nằm 34 chỗ")}
+                    checked={selectedCheckboxes.includes(
+                      "Xe giường nằm 34 chỗ"
+                    )}
                   />
                   <FormControlLabel
                     control={<Checkbox {...label} />}
                     label="Xe giường nằm 40 chỗ"
                     value="Xe giường nằm 40 chỗ"
                     onChange={handleCheckboxChange}
-                    checked={selectedCheckboxes.includes("Xe giường nằm 40 chỗ")}
+                    checked={selectedCheckboxes.includes(
+                      "Xe giường nằm 40 chỗ"
+                    )}
                   />
                 </FormGroup>
               </Item>
+              <Item className="cnt__filter">
+                <Typography className="">Giá vé:</Typography>
+              </Item>
             </Grid>
             <Grid item xs={8}>
-              <Item>
-                {filteredData?.map((items, index) => (
-                  <InfoTrip items={items} key={index} />
-                ))}
+              <div className="cnt__info">
+                {showNumber ? (
+                  <span className="cnt__info">
+                    {" "}
+                    Thông tin các chuyến xe đi từ "Nơi bắt đầu" đến "Nơi đến"
+                    gồm: {fetchData.length}
+                  </span>
+                ) : (
+                  <Skeleton variant="text" sx={{ fontSize: "1.5rem" }} />
+                )}
+              </div>
+              <Item className="cnt__listtrip">
+                {showNumber ? (
+                  filteredData?.map((items, index) => (
+                    <InfoTrip items={items} key={index} />
+                  ))
+                ) : (
+                  <SkeletonCustom />
+                )}
               </Item>
             </Grid>
           </Grid>
-        </Box>
-      </Container>
+        </Container>
+      </div>
     </React.Fragment>
   );
 };
