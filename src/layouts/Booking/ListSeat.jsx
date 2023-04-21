31086e779b5f8895dc9fd40f62b18d51a0374179
LV NT <levnhattan@gmail.com>
2023-04-22 00:38:25 +0700

    [Fix] resolve conflicts listseat.jsx

--- src/layouts/Booking/ListSeat.jsx
+++ src/layouts/Booking/ListSeat.jsx
@@ -153,33 +153,6 @@ const ListSeat = ({ items }) => {
     </TableCell>
   );
 
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-
-  const handleContinue = () => {
-    console.log("data: " + JSON.stringify(items));
-
-    if (totalPrice === 0) {
-      console.log("1232121");
-    } else {
-      dispatch(
-        tripActions.addBooking({
-          id: items.id,
-          IdTrip: items.ID_Trip,
-          NameGarage: items.NameGarage,
-          NameTrip: items.NameTrip,
-          StartTime: items.StartTime,
-          PakingStart: items.PakingStart,
-          PakingEnd: items.PakingEnd,
-          price: items.Price,
-          totalSeat: selectedSeatNames.length,
-          listSeated: selectedSeatNames,
-          totalPrice,
-        })
-      );
-        navigate("/payment");
-    }
-  };
 
   const SeatTable = ({ choNgoi }) => (
     <div className="table">
@@ -202,14 +175,10 @@ const ListSeat = ({ items }) => {
         </Table>
       </TableContainer>
       <div>
-        <span>Danh sách ghế đang đặt: </span>
-        {selectedSeatNames.map((name, index) => (
-          <span key={index}>
-            {name}
-            {index !== selectedSeatNames.length - 1 && ", "}
-           </span>
-        ))}
-       
+      <span>Ghế đang đặt:</span>
+        {selectedSeatNames.map((name) => (
+          <span key={name}>{name} </span>
+          ))}
         <br />
         <span>Tổng số lượng ghế: {selectedSeatNames.length}</span>
       </div>
@@ -228,11 +197,7 @@ const ListSeat = ({ items }) => {
     </div>
   );
 
-  return (
-    <>
-      <SeatTable choNgoi={choNgoi} />
-    </>
-  );
+  return <SeatTable choNgoi={choNgoi} />;
 };
 
 export default ListSeat;
