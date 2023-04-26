85a9ff8af13165f2f254937ec267c1d183265820
LV NT <levnhattan@gmail.com>
2023-04-26 20:47:46 +0700

    [Add] Linking search with booking

--- src/layouts/Booking/Booking.js
+++ src/layouts/Booking/Booking.js
@@ -8,35 +8,49 @@ import {
   query,
   onSnapshot,
   where,
+  Timestamp ,
 } from "firebase/firestore";
 import { db } from "./../../data/firebase";
 
+import dayjs from "dayjs";
+
 const Booking = () => {
   const [data, setData] = useState([]);
+  const stateSearch = useSelector(state => state.trip.stateSearch);
   const dispatch = useDispatch();
 
-  // const stateSearch = useSelector(state => state.trip.stateSearch);
-  // console.log(dispatch(tripActions.addSearch()))
-  // console.log("stateSearch: " + stateSearch);
+  const StartPoint = stateSearch[0].origin;
+  const EndPoint = stateSearch[0].destination;
+  const selectDate = stateSearch[0].selectDate;
+  // console.log(StartPoint,EndPoint, selectDate)
+  // const isoDate1 = Timestamp.fromDate(selectDate.toDate());
+  // console.log("isoDate1: " + isoDate1)
+
+  const addedDate = dayjs(selectDate).add(10, 'hour').toDate();
+  const isoDate = Timestamp.fromDate(addedDate);
+  console.log("isoDate: " + isoDate)
 
   useEffect(() => {
-    const q = query(collection(db, "Trips")); //,where("ID_Car","==", "car01")
+    const q = query(collection(db, "Trips"), where("StartPoint", "==", StartPoint),
+     where("EndPoint", "==", EndPoint), where("StartTime", ">", isoDate ) ); // 
     const unsubscribe = onSnapshot(q, (querySnapshot) => {
       let tempDB = [];
       querySnapshot.forEach((doc) => {
-        tempDB.push({ ...doc.data() , id: doc.id});
+        tempDB.push({ ...doc.data(), id: doc.id });
+
       });
-      // console.log("data Trips from firestore: ", tempDB);
+      console.log(tempDB);
       setData(tempDB);
-    });
+    })
     return () => unsubscribe();
   }, []);
-  
+
+
   return (
     <>
-    <Header />
-    <Trip fetchData={data} />
-    <Footer />
+      <Header />
+      <Trip fetchData={data} />
+      <Footer />
     </>
   )
 }
