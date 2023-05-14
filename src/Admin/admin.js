d5fffa5696370f7ea9de6e347be62221cc8eb244
Manh <dvfbxm@example.com>
2023-05-14 15:04:24 +0700

    [Modified] Xanceel Tickets

--- src/Admin/admin.js
+++ src/Admin/admin.js
@@ -30,6 +30,7 @@ import Garage from "./layouts/Garage/Garage";
 import Tables from "./layouts/tables";
 import ListCar from "./layouts/ListCar/ListCar";
 import { ManagerTickets } from "./layouts/ManagerTickets/ManagerTickets";
+import CancelTickets from "./layouts/CancelTickets/CancelTickets";
 
 
 export default function Admin() {
@@ -131,7 +132,7 @@ export default function Admin() {
     setSelectedValue(value);
   };
   const renderComponent = () => {
-    return selectedValue === 1 ? <Dashboard /> : (selectedValue === 2 ? <UserTable /> : (selectedValue === 3 ? <Garage /> : (selectedValue === 4 ? <ListCar /> : <ManagerTickets/>)));
+    return selectedValue === 1 ? <Dashboard /> : (selectedValue === 2 ? <UserTable /> : (selectedValue === 3 ? <Garage /> : (selectedValue === 4 ? <ListCar /> : (selectedValue === 5 ? <ManagerTickets /> : <CancelTickets/>))));
   };
   return (
     <>
