171d836362bfddaedffe241a1b6838dd8a8e2260
Manh <dvfbxm@example.com>
2023-05-18 10:18:08 +0700

    [Modified]UI form user

--- src/layouts/Form/User/UserForm.jsx
+++ src/layouts/Form/User/UserForm.jsx
@@ -7,9 +7,11 @@ import Box from "@mui/material/Box";
 import { useNavigate } from "react-router-dom";
 import { auth, db } from "data/firebase";
 import FormTicket from "layouts/Form/FormTicket";
-import FormInfo from "layouts/Form/FormInfo";
+
 import DashboardLayout from "Admin/examples/LayoutContainers/DashboardLayout";
 import { Button, Grid } from "@mui/material";
+import FormInfo from "../FormInfo/FormInfo";
+import FormPassWord from "../FormPassWord/FormPassWord";
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
 
@@ -61,13 +63,15 @@ export default function UserForm() {
       <Grid container rowSpacing={1} style={{ width: "100%", minHeight: "600px", paddingTop:'6rem' }}>
         <Grid xs={2} style={{ display: "grid" }}>
           <Button onClick={() => setValueTab(1)}>Thông tin cá nhân</Button>
-          <Button onClick={() => setValueTab(3)}>Lịch sử đặt vé</Button>
+          <Button onClick={() => setValueTab(2)}>Lịch sử đặt vé</Button>
+          <Button onClick={() => setValueTab(3)}>Đổi mật khẩu</Button>
+
         </Grid>
         <Grid
           xs={10}
           style={{ width: "100%", height: "100%", border: "1px solid black" }}
         >
-          {valueTab === 1 ? <FormInfo /> : <FormTicket />}
+          {valueTab === 1 ? <FormInfo /> : (valueTab === 2 ? <FormTicket /> : <FormPassWord />)}
         </Grid>
       </Grid>
     </>
